8adb75ca369a14512cd1be2d5ce0d8e77f776342
Kat Jansen <17cosmiclane@gmail.com>
2019-11-15 21:54:27 -0600

    Calculate greatest increase and decrease

--- PyBank/main.py
+++ PyBank/main.py
@@ -4,33 +4,52 @@ import csv
 #define column variables as arrays
 date = []
 profit_loss = []
+change = []
 
+###Pull Data from CSV into workable forms
 #open and read budget_data
-#my csv has headers, do I need to get rid of these?
 with open('Resources/budget_data.csv', newline='') as csvfile:
     budget_data = csv.reader(csvfile)
 #add data from csv to arrays
-#I need to remove the headers, they're in these arrays and they shouldn't be
     for row in budget_data:
         date.append(row[0])
         profit_loss.append(row[1])
+#remove headers from arrays
     date.pop(0)
     profit_loss.pop(0)
-    profit_loss = [ int(val) for val in profit_loss]
+#cast all elements of profit_loss as integers
+    profit_loss = [ int(row) for row in profit_loss]
+#capture changes in profit_loss
+#len -2 to exclude final element
+    for i in range(len(profit_loss)-2):
+        change.append(profit_loss[i+1] - profit_loss[i])
 
+###Begin Analysis
 #count months (rows) in date array
-    months = sum(1 for row in date)-1
+    months = sum(1 for row in date)
 #sum Profit/Losses Column
     net = sum(profit_loss)
-#average Profit/Losses Column
+#average change array, rounded to hundredths place
 #this is not returning the correct value
-    avg_change = net/len(profit_loss)
+    avg_change = round(sum(change)/len(change),2)
+#Find value/index/date of max value in change array
+#+1 to account for shift when calculating diff between elements
+    max_change = max(change)
+    max_index = change.index(max_change) +1
+    max_row = date[max_index] + " ($"+str(max_change)+")"
+
+#Find value/index/date of min value in change array
+#+1 to account for shift when calculating diff between elements
+    min_change = min(change)
+    min_index = change.index(min_change) +1
+    min_row = date[min_index] + " ($"+str(min_change)+")"
+
 
 #print statement for testing
-print(avg_change)
+print(min_row)
 
 
-# #print Financial Analysis
+# ##print Financial Analysis
 #     print("Financial Analysis")
 #     print("--------------------------------")
 #     print("Total Months: "+ str(months))
