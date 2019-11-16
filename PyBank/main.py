45fd8fcdfb6d2a873300820182bccfe1d860d7f9
Kat Jansen <17cosmiclane@gmail.com>
2019-11-15 21:33:58 -0600

    Add all final print statements. Add Average Change

--- PyBank/main.py
+++ PyBank/main.py
@@ -14,11 +14,27 @@ with open('Resources/budget_data.csv', newline='') as csvfile:
     for row in budget_data:
         date.append(row[0])
         profit_loss.append(row[1])
+    date.pop(0)
+    profit_loss.pop(0)
+    profit_loss = [ int(val) for val in profit_loss]
 
-#count months (rows) in budget_data
+#count months (rows) in date array
     months = sum(1 for row in date)-1
+#sum Profit/Losses Column
+    net = sum(profit_loss)
+#average Profit/Losses Column
+#this is not returning the correct value
+    avg_change = net/len(profit_loss)
+
+#print statement for testing
+print(avg_change)
+
 
 # #print Financial Analysis
 #     print("Financial Analysis")
 #     print("--------------------------------")
-    print("Total Months: "+ str(months))
+#     print("Total Months: "+ str(months))
+#     print("Total: $"+str(net))
+#     print("Average Change: ")
+#     print("Greatest Increase in Profits: ")
+#     print("Greatest Decrease in Profits:")
