2a981057f585dfba2d5a98f3b0f98b925ddb42e9
Kat Jansen <17cosmiclane@gmail.com>
2019-11-15 21:07:32 -0600

    Add comments to existing code

--- PyBank/main.py
+++ PyBank/main.py
@@ -1,5 +1,13 @@
+#import csv methods
 import csv
+#open and read budget_data
 with open('Resources/budget_data.csv', newline='') as csvfile:
     budget_data = csv.reader(csvfile, delimiter=' ', quotechar='|')
+   
+#count months (rows) in budget_data
     months = sum(1 for row in budget_data)-1
+    
+#print Financial Analysis
+    print("Financial Analysis")
+    print("--------------------------------")
     print("Total Months: "+ str(months))
