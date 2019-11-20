0c405884187456551db59e30be3c7a48bca3f507
Kat Jansen <17cosmiclane@gmail.com>
2019-11-19 21:20:25 -0600

    Split name into First Name and Last Name, replace name attribute with these new attributes.

--- PyBoss/main.py
+++ PyBoss/main.py
@@ -1,6 +1,16 @@
 import csv
 
-with open('employee_data.csv','r') as csv:
-    data = csv.reader(csv)
-    header = next(data)
-    
+with open('Employee_Analysis.csv','w+') as output:
+    out_header = ['Emp ID','First Name','Last Name','DOB','SSN','State\n']
+    writer = csv.writer(output)
+    writer.writerow(out_header)
+    new_row = []
+    with open('employee_data.csv','r') as input:
+        data = csv.reader(input)
+        in_header = next(data)
+        row = next(data)
+        new_row = row[:]
+        name = new_row.pop(1).split(' ')
+        new_row.insert(1,name[0])
+        new_row.insert(2,name[1])
+        print(new_row)
