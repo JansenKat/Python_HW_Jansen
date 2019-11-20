11fe33fd682dbd83ecad751bb1b711e9d5866d97
Kat Jansen <17cosmiclane@gmail.com>
2019-11-19 21:28:46 -0600

    Create output file with split name

--- PyBoss/main.py
+++ PyBoss/main.py
@@ -5,12 +5,13 @@ with open('Employee_Analysis.csv','w+') as output:
     writer = csv.writer(output)
     writer.writerow(out_header)
     new_row = []
-    with open('employee_data.csv','r') as input:
-        data = csv.reader(input)
+    with open('employee_data.csv','r') as in_data:
+        data = csv.reader(in_data)
         in_header = next(data)
-        row = next(data)
-        new_row = row[:]
-        name = new_row.pop(1).split(' ')
-        new_row.insert(1,name[0])
-        new_row.insert(2,name[1])
-        print(new_row)
+        for row in data:
+            new_row = row[:]
+            name = new_row.pop(1).split(' ')
+            new_row.insert(1,name[0])
+            new_row.insert(2,name[1])
+            writer.writerow(new_row)
+    
