601dc5ae53ae13087da4818bff3e845cd4f0a5e6
Kat Jansen <17cosmiclane@gmail.com>
2019-11-21 18:41:53 -0600

    Convert reader and writer to DictReader and Writer

--- PyBoss/main.py
+++ PyBoss/main.py
@@ -1,13 +1,16 @@
 import csv
 
-with open('Employee_Analysis.csv','w+') as output:
-    out_header = ['Emp ID','First Name','Last Name','DOB','SSN','State\n']
-    writer = csv.writer(output)
-    writer.writerow(out_header)
-    new_row = []
+with open('Employee_Analysis.csv','w+') as clean:
+    fields = ['Emp ID','First Name','Last Name','DOB','SSN','State']
+    writer = csv.DictWriter(clean, fieldnames=fields)
+    writer.writeheader()
+    new_row={}
+    
     with open('employee_data.csv','r') as in_data:
-        data = csv.reader(in_data)
-        in_header = next(data)
+        data = csv.DictReader(source)
+        full_name = []
+        #Read, split name, and write row
+        #may be able to ise dict or list comprehension for this
         for row in data:
             new_row = row[:]
             name = new_row.pop(1).split(' ')
