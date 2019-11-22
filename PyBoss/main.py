6eb12e0231089ca0ff0a6dfc2b3d93c690fca47c
Kat Jansen <17cosmiclane@gmail.com>
2019-11-21 19:16:35 -0600

    Hide first 5 digits of SSN by overwriting them

--- PyBoss/main.py
+++ PyBoss/main.py
@@ -1,57 +1,12 @@
 import csv
-import datetime
+import datetime as dt
 
-us_state_abbrev = {
-    'Alabama': 'AL',
-    'Alaska': 'AK',
-    'Arizona': 'AZ',
-    'Arkansas': 'AR',
-    'California': 'CA',
-    'Colorado': 'CO',
-    'Connecticut': 'CT',
-    'Delaware': 'DE',
-    'Florida': 'FL',
-    'Georgia': 'GA',
-    'Hawaii': 'HI',
-    'Idaho': 'ID',
-    'Illinois': 'IL',
-    'Indiana': 'IN',
-    'Iowa': 'IA',
-    'Kansas': 'KS',
-    'Kentucky': 'KY',
-    'Louisiana': 'LA',
-    'Maine': 'ME',
-    'Maryland': 'MD',
-    'Massachusetts': 'MA',
-    'Michigan': 'MI',
-    'Minnesota': 'MN',
-    'Mississippi': 'MS',
-    'Missouri': 'MO',
-    'Montana': 'MT',
-    'Nebraska': 'NE',
-    'Nevada': 'NV',
-    'New Hampshire': 'NH',
-    'New Jersey': 'NJ',
-    'New Mexico': 'NM',
-    'New York': 'NY',
-    'North Carolina': 'NC',
-    'North Dakota': 'ND',
-    'Ohio': 'OH',
-    'Oklahoma': 'OK',
-    'Oregon': 'OR',
-    'Pennsylvania': 'PA',
-    'Rhode Island': 'RI',
-    'South Carolina': 'SC',
-    'South Dakota': 'SD',
-    'Tennessee': 'TN',
-    'Texas': 'TX',
-    'Utah': 'UT',
-    'Vermont': 'VT',
-    'Virginia': 'VA',
-    'Washington': 'WA',
-    'West Virginia': 'WV',
-    'Wisconsin': 'WI',
-    'Wyoming': 'WY',
+us_state_abbrev = {'Alabama': 'AL','Alaska': 'AK','Arizona': 'AZ','Arkansas': 'AR','California': 'CA','Colorado': 'CO','Connecticut': 'CT','Delaware': 'DE',
+    'Florida': 'FL','Georgia': 'GA','Hawaii': 'HI','Idaho': 'ID','Illinois': 'IL','Indiana': 'IN','Iowa': 'IA','Kansas': 'KS','Kentucky': 'KY','Louisiana': 'LA',
+    'Maine': 'ME','Maryland': 'MD','Massachusetts': 'MA','Michigan': 'MI','Minnesota': 'MN','Mississippi': 'MS','Missouri': 'MO','Montana': 'MT','Nebraska': 'NE',
+    'Nevada': 'NV','New Hampshire': 'NH','New Jersey': 'NJ','New Mexico': 'NM','New York': 'NY','North Carolina': 'NC','North Dakota': 'ND','Ohio': 'OH',
+    'Oklahoma': 'OK','Oregon': 'OR','Pennsylvania': 'PA','Rhode Island': 'RI','South Carolina': 'SC','South Dakota': 'SD','Tennessee': 'TN','Texas': 'TX',
+    'Utah': 'UT','Vermont': 'VT','Virginia': 'VA','Washington': 'WA','West Virginia': 'WV','Wisconsin': 'WI','Wyoming': 'WY',
     }
 
 with open('clean_employee_data.csv','w+') as clean:
@@ -62,20 +17,24 @@ with open('clean_employee_data.csv','w+') as clean:
     
     with open('employee_data.csv','r') as source:
         data = csv.DictReader(source)
-        full_name = []
+        
         #Read, split name, and write row
-        #may be able to ise dict or list comprehension for this
         for row in data:
             #copy the row
             new_row = row.copy()
+
             #Separate first and last name
             full_name = new_row.pop('Name')
             full_name = full_name.split(' ')
             new_row['First Name'] = full_name[0]
             new_row['Last Name'] = full_name[1]
+
             #Convert date format with datetime
-            new_row['DOB'] = datetime.datetime.strptime(new_row['DOB'], '%Y-%m-%d').strftime('%m/%d/%y')
+            new_row['DOB'] = dt.datetime.strptime(new_row['DOB'], '%Y-%m-%d').strftime('%m/%d/%y')
+
             #Hide first 5 digits of SSN
+            new_row['SSN'] = '***-**-' + new_row['SSN'][-4:]
+
             #Convert state to ISO code
             new_row['State'] = us_state_abbrev[new_row['State']]
             writer.writerow(new_row)
